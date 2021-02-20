f1c0253f248c47e20a6414d625a2ccf17fa40d16
doug <doug@mx1>
2021-02-19 23:27:33 -0500

    initial sitepoint setup

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+import FancyParagraph from "../components/fancy-paragraph"
+
+
+const AboutPage = () => (
+<Layout>
+ <h1>About Me</h1>
+ <FancyParagraph paragraphText="Styled with CSS Modules." />
+</Layout>
+)
+
+
+export default AboutPage
+
